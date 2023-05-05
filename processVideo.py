116af770568b891e3999c72993a2887d004dd2cf
Adithya Yerramsetty <adithya.yerramsetty@gmail.com>
2023-05-05 12:15:05 -0700

    Built panoptic segentation code, with disk dumps.

--- processVideo.py
+++ processVideo.py
@@ -8,11 +8,20 @@ a prediction of object
 locations in the scene using
 Mask2Former + Cross Frame Matching
 """
+import os
+from datetime import datetime, timedelta
+import json
+
+from transformers import AutoImageProcessor, Mask2FormerForUniversalSegmentation
+import torch
 import imageio.v3 as iio
 from PIL import Image
-import os
+
+panopticProcessor = AutoImageProcessor.from_pretrained("facebook/mask2former-swin-base-coco-panoptic")
+panopticModel = Mask2FormerForUniversalSegmentation.from_pretrained("facebook/mask2former-swin-base-coco-panoptic")
 
 COLMAP_INPUT_DIR = "colmapIn"
+SEMANTIC_RESULT_DIR = "semanticOut"
 
 def videoFileToImages(videoFileName, frameSkip) -> None:
     """
@@ -42,6 +51,45 @@ def videoFileToImages(videoFileName, frameSkip) -> None:
             # Convert to image
             pilImage = Image.fromarray(frame)
             pilImage.save(f"{COLMAP_INPUT_DIR}/{frameCount}.jpg")
+        
+def runPanopticSegmentation():
+    """
+    Runs panoptic segmentation
+    on all extracted frames,
+    and dumps the results
+    to disk on a separate
+    folder
+    """
+    fileNames = os.listdir(COLMAP_INPUT_DIR)
+
+    # Create the results directory if it doesn't exist
+    if not os.path.exists(SEMANTIC_RESULT_DIR):
+        os.makedirs(SEMANTIC_RESULT_DIR)
+
+    for fileName in fileNames:
+        image = Image.open(f"{COLMAP_INPUT_DIR}/{fileName}")
+        panopticInputs = panopticProcessor(image, return_tensors="pt")
+        with torch.no_grad():
+            panopticOutputs = panopticModel(**panopticInputs)
+        
+        panopticPrediction = panopticProcessor.post_process_panoptic_segmentation(panopticOutputs, target_sizes=[image.size[::-1]])[0]
+
+        # Need to transpose the segmentation mask to get the right shape
+        # Also, convert it to a standard python list
+        pantopicSementationMatrixAsList = panopticPrediction["segmentation"].T.tolist()
+        # Contains a dict that stores assignments from image instance id to
+        # global class id
+        panopticSementationAssignments = panopticPrediction["segments_info"]
+
+        # Store both in a dict, and save to disk
+        panopticResultDict = {
+            "segmentationList": pantopicSementationMatrixAsList,
+            "assignments": panopticSementationAssignments
+        }
+
+        with open(f"{SEMANTIC_RESULT_DIR}/{fileName}.json", "w") as f:
+            json.dump(panopticResultDict, f)
+
 
 def runOnVideoFile(videoFileName, frameSkip=30):
     """
@@ -58,7 +106,8 @@ def runOnVideoFile(videoFileName, frameSkip=30):
        too small and you might get ambiguities due
        to low parralax between frames.
     """
-    videoFileToImages(videoFileName, frameSkip)
+    # videoFileToImages(videoFileName, frameSkip)
+    runPanopticSegmentation()
 
 
 runOnVideoFile("inputvid.mp4")
